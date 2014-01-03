b6dd98eacbb4774c66e926f1f667808be279fc99
dwalldorf <daniel.walldorf@devbliss.com>
2014-01-03 12:47:02 +0100

    Added some git specific aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,5 +2,9 @@
 alias ll='ls -al'
 alias df='df -h'
 
+# git stuff
 alias gd='git diff --color'
 alias gdc='git diff --cached --color'
+alias master='git checkout master'
+alias feme='git fetch && git merge origin/master'
+alias gs='git submodule update --recursive'
