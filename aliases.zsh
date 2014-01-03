31c92f18707679ec0824645aefc8311d9b25a2a9
dwalldorf <daniel.walldorf@devbliss.com>
2014-01-03 12:43:31 +0100

    Renamed files to end at .zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,6 @@
 # aliases
 alias ll='ls -al'
 alias df='df -h'
+
 alias gd='git diff --color'
+alias gdc='git diff --cached --color'
--- zprofile.zsh
+++ zprofile.zsh
@@ -11,4 +11,4 @@ ZSH_THEME="apple"
 plugins=(osx git)
 
 source $ZSH/oh-my-zsh.sh
-source aliases
+source aliases.zsh
