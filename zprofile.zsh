97cbb2087668372ca5ef3767ffc7669487a8fa92
dwalldorf <daniel.walldorf@devbliss.com>
2014-01-13 17:07:06 +0100

    Fixed Maven paths

--- zprofile.zsh
+++ zprofile.zsh
@@ -1,6 +1,11 @@
 # Path to your oh-my-zsh configuration.
-export M3_HOME=/Applications/apache-maven-3.0.5/bin/mvn
-export PATH=$HOME/bin:$HOME/.my-zsh/bin:/usr/local/bin:$M3_HOME:$PATH
+
+export M3_HOME="/Applications/apache-maven-3.0.5/"
+export M3=$M3_HOME/bin
+export M2_HOME=""
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.6.0_37-b06-434.jdk/Contents/Home/"
+
+export PATH=$HOME/bin:$HOME/.my-zsh/bin:/usr/local/bin:$M3_HOME:$M3:$JAVA_HOME:$PATH
 
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="apple"
