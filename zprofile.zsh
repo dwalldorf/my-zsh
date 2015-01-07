a8a6ab89899d5d9b35f0bfda1f18f86d3a7cc95c
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-01-07 15:58:13 +0100

    Key bindings for numpad

--- zprofile.zsh
+++ zprofile.zsh
@@ -1,11 +1,12 @@
 # Path to your oh-my-zsh configuration.
 
+export GRADLE_HOME="/usr/bin/gradle/current"
 export M3_HOME="/Applications/apache-maven-3.0.5/"
 export M3=$M3_HOME/bin
 export M2_HOME=""
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/current/Contents/Home/"
 
-export PATH=$HOME/bin:$HOME/.my-zsh/bin:/usr/local/bin:$M3_HOME:$M3:$JAVA_HOME:$PATH
+export PATH=$HOME/bin:$HOME/.my-zsh/bin:/usr/local/bin:$M3_HOME:$M3:$JAVA_HOME:$PATH:$GRADLE_HOME/bin
 
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="apple"
@@ -17,3 +18,27 @@ plugins=(osx git)
 
 source $ZSH/oh-my-zsh.sh
 source ~/.my-zsh/aliases.zsh
+
+
+# Keypad
+# 0 . Enter
+bindkey -s "^[Op" "0"
+bindkey -s "^[Ol" "."
+bindkey -s "^[OM" "^M"
+# 1 2 3
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+# 4 5 6
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+# 7 8 9
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+# + -  * /
+bindkey -s "^[Ok" "+"
+bindkey -s "^[Om" "-"
+bindkey -s "^[Oj" "*"
+bindkey -s "^[Oo" "/"
