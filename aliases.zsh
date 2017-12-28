157e4e662edfe752b0840d57ffe1db1831496d07
Daniel Walldorf <dwalldorf@me.com>
2017-12-28 09:57:04 +0100

    cleanup

--- aliases.zsh
+++ aliases.zsh
@@ -45,7 +45,5 @@ alias gsp='gs pop'
 alias gsr='gs && gfr && gsp'
 
 # Springer
-
 alias cm-pass='PASSWORD_STORE_DIR=~/.cm-pass pass'
-
 alias vpn-connect='sudo openconnect -u dwn0325 --authgroup=SpringerNature https://vpn.springer-sbm.com'
